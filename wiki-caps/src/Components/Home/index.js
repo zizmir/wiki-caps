ef61b684e45bc1e7293d4d15a6e0cdc2b1a030cd
m'bark <mbark.bouznir@gmail.com>
2017-12-01 11:55:25 +0100

    add img and json

--- wiki-caps/src/Components/Home/index.js
+++ wiki-caps/src/Components/Home/index.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import caps from 'caps.json';
 
 
 class Home extends Component {
-    render() {
-        return (<h1>Home</h1>);
+
+    constructor(){
+        super();
     }
+    render() {
+        <h4>Random caps of the moment</h4>
+        console.log(caps)
+        caps.map((item, index) => {
+
+        })}
+
+
 }
 
 export default Home;
