ca9bf7bc9da967d0964ebf816ca2a67db324aa92
m'bark <mbark.bouznir@gmail.com>
2017-12-01 14:31:52 +0100

    error 404

--- wiki-caps/src/Components/Error404/index.js
+++ wiki-caps/src/Components/Error404/index.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
+import errorGif from './404_gif_2.gif';
 
 class Error404 extends Component {
     render() {
         return (
-            <h1>Erreur 404</h1>
+            <div >
+            <img className="center-block" src={errorGif} />
+            </div>
         );
     }
 }
