58e56e67cfa855e7412534cfd414b7983c9b11ce
vincent <vincent.graul@outlook.com>
2017-12-01 12:33:30 +0100

    navbar routing

--- wiki-caps/src/Components/Navbar/index.js
+++ wiki-caps/src/Components/Navbar/index.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Salut</h1>
-            </div>
+            <nav className="navbar">
+                <Router>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/gallery">Gallery</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </Router>
+            </nav>
         );
     }
 }
